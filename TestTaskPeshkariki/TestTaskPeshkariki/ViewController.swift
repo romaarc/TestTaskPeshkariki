88eee7090fd4668e68e33deded302f8871343e73
Roman <romangorshkovreme@gmail.com>
2022-04-29 14:32:12 +0300

    first

---
+++ TestTaskPeshkariki/TestTaskPeshkariki/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TestTaskPeshkariki
+//
+//  Created by Roman Gorshkov on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
+
